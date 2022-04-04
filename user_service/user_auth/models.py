2bcba1e8818588ecd6853a69d4be99df097a1014
nardoz-dev <nardomen96@gmail.com>
2022-04-04 10:39:09 +0200

    Adding new migration

--- user_service/user_auth/models.py
+++ user_service/user_auth/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
     date_of_birth = models.DateField(default=datetime.date(1977, 1, 1))
-    phone = models.CharField( max_length=10 )
+    phone = models.CharField( default= ' ' , max_length=10 )
     billing_address = models.CharField(default = ' ' , max_length = 60)
     shipping_address = models.CharField(default = ' ' , max_length = 60)
 
