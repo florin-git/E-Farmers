57e0836baca1bf79736463e562ce818a86b2aaa7
Davide Bazzana <db.bazzana@gmail.com>
2022-05-21 15:21:49 +0200

    Containerized frontend

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -100,16 +100,11 @@ WSGI_APPLICATION = 'user_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        # 'USER': os.getenv('DATABASE_USER'),
-        # 'NAME': os.getenv('DATABASE_NAME'),
-        # 'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        # 'HOST': os.getenv('DATABASE_HOSTNAME'),
-        # 'PORT': os.getenv('DATABASE_PORT'),
-        'USER': 'efarmers',
-        'NAME': 'efarmers',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'USER': os.getenv('DATABASE_USER'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOSTNAME'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
