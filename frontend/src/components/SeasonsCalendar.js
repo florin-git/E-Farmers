2e8c3b963b0870c0da0522bf2011f74ac52cb4f5
Florin <flocuconasu10@gmail.com>
2022-07-24 10:34:09 +0200

    Update SeasonsCalendar.js
    I simply added a key value to each <li> element in such a way that the warning "Each child in a list should have a unique 'key' prop.", doesn't appear.

--- frontend/src/components/SeasonsCalendar.js
+++ frontend/src/components/SeasonsCalendar.js
@@ -17,53 +17,55 @@ seasonal_food.set('Fall', ["Apples", "Cranberries", "Figs", "Grapes", "Pears", "
 
 const seasons = new Map();
 for (const x of Array(12).keys()) {
-    if (x < 2 || x == 11) {
-        seasons.set(x, 'Winter');
-    } else if (x < 5) {
-        seasons.set(x, 'Spring');
-    } else if (x < 8) {
-        seasons.set(x, 'Summer');
-    } else {
-        seasons.set(x, 'Fall');
-    }
+  if (x < 2 || x === 11) {
+    seasons.set(x, "Winter");
+  } else if (x < 5) {
+    seasons.set(x, "Spring");
+  } else if (x < 8) {
+    seasons.set(x, "Summer");
+  } else {
+    seasons.set(x, "Fall");
+  }
 }
-
+const today = new Date();
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
 class SeasonsCalendar extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-        // logs: [],
-        value: [],
-        };
+  constructor() {
+    super();
+    this.state = {
+      // logs: [],
+      value: [],
+    };
+  }
+
+  // {this.state.logs.map(log => <div key={log}>{log}</div>)}
+  addLog(date) {
+    // this.setState({ logs: this.state.logs.concat([message]) });
+    this.setState({ value: seasonal_food.get(seasons.get(date)) });
+  }
+
+  render() {
+    const seasonal_items = [];
+    
+    for (const item of this.state.value) {
+        seasonal_items.push(<li >{item}</li>);
     }
 
-    // {this.state.logs.map(log => <div key={log}>{log}</div>)}
-    addLog(date) {
-        // this.setState({ logs: this.state.logs.concat([message]) });
-        this.setState({ value: seasonal_food.get(seasons.get(date))})
-    }
-
-    render() {
-        const seasonal_items = [];
-        for (const item of this.state.value){
-            seasonal_items.push(<li>{item}</li>)
-        }
-        return (
-        <div>
-            <div id="events-log">
-            <h3>The calendar of seasonal food</h3>
-            </div>
-            <Calendar
-            minDate={new Date()}
-            enableRangeSelection={true}
-            onDayClick={e => this.addLog(e.date.getMonth())}
-            />
-            <div>
-            {seasonal_items}
-            </div>
+    return (
+      <div>
+        <div id="events-log">
+          <h3>The calendar of seasonal food</h3>
         </div>
-        );
-    }
-}  
+        <Calendar
+          minDate={new Date()}
+          enableRangeSelection={true}
+          onDayClick={(e) => this.addLog(e.date.getMonth())}
+        />
+        <div>{seasonal_items}</div>
+      </div>
+    );
+  }
+}
 
 export default SeasonsCalendar;
