14048b84263e7ef19d374d694ee644b3bfbbde10
Davide Bazzana <db.bazzana@gmail.com>
2022-07-30 15:05:31 +0200

    Subscription service restful api

--- subscription_service/app.py
+++ subscription_service/app.py
@@ -1,73 +1,77 @@
-from email import message
+# from email import message
 from flask import Flask
 from flask import request
+from flask_restful import Resource, Api
 from threading import Timer
 import _thread
 import pika
+import json
 
 app = Flask(__name__)
+api = Api(app)
 
 def callback(messages, ch, method, properties, body):
-    print(" [x] %r" % body)
-    messages.append(body)
-    print(messages)
+    message = body.decode()
+    messages.append(message)
     if messages[0] == 1:
         ch.stop_consuming()
     messages[0] -= 1
 
-@app.route("/register-queue", methods=['GET'])
-def register_queue():
-    # Create a new queue in RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue="user_" + request.args.get('user-id'), exclusive=False, durable=True)
-    connection.close()
-    return "<p>New queue registered</p>"
+class Queue(Resource):
+    def put(self):
+        # Create a new queue in RabbitMQ
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        channel = connection.channel()
+        queue = channel.queue_declare(queue="user_" + request.form['user_id'], exclusive=False, durable=True)
+        print(queue)
+        connection.close()   
+        return 201
+    def patch(self, user_id):
+        # Create a new binding between a queue and an exchange
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        channel = connection.channel()
+        channel.queue_bind(exchange="farmer_" + request.form['farmer_id'], queue="user_" + user_id)
+        connection.close()
+        return 200
+    def get(self, user_id):
+        # Read all the messages in the queue
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        channel = connection.channel()
+        queue_name = "user_" + user_id
+        queue = channel.queue_declare(queue=queue_name, exclusive=False, durable=True)
+        messages = [queue.method.message_count]
+        if messages[0] > 0:
+            channel.basic_consume(
+                queue=queue_name,
+                on_message_callback=lambda *args, **kwargs: callback(messages, *args, **kwargs), 
+                auto_ack=True)
+            channel.start_consuming()
+        connection.close()
+        return json.dumps(messages[1:])
 
-@app.route("/register-farmer", methods=['GET'])
-def register_farmer():
-    # Create a new queue in RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
-    channel = connection.channel()
-    channel.exchange_declare(exchange="farmer_" + request.args.get('farmer-id'), exchange_type='fanout')
-    connection.close()
-    return "<p>New farmer registered</p>"
+class Exchange(Resource):
+    def put(self):
+        # Create a new exchange in RabbitMQ
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        channel = connection.channel()
+        channel.exchange_declare(exchange="farmer_" + request.form['farmer_id'], exchange_type='fanout')
+        connection.close()
+        return 201
+    def post(self, farmer_id):
+        # Deliver a message to the exchange
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        channel = connection.channel()
+        message = request.form['message']
+        channel.basic_publish(exchange="farmer_" + farmer_id,
+                        routing_key='',
+                        body=message,
+                        properties=pika.BasicProperties(
+                            delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE))
+        connection.close()
+        return 200
 
-@app.route("/subscribe", methods=['GET'])
-def subscribe():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_bind(exchange="farmer_" + request.args.get('farmer-id'), queue="user_" + request.args.get('user-id'))
-    connection.close()
-    return "<p>New subscription registered</p>"
+api.add_resource(Queue, '/customer', '/customer/<string:user_id>')
+api.add_resource(Exchange, '/farmer', '/farmer/<string:farmer_id>')
 
-@app.route("/publish", methods=['GET'])
-def publish():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    message = request.args.get('message')
-    channel.basic_publish(exchange="farmer_" + request.args.get('farmer-id'),
-                      routing_key='',
-                      body=message,
-                      properties=pika.BasicProperties(
-                        delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE))
-    print(" [x] Sent %r" % message)
-    connection.close()
-    return "<p>New message published</p>"
-
-@app.route("/retrieve-messages", methods=['GET'])
-def retrieve_messages():
-    # Retrieve messages from farmers
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    queue_name = "user_" + request.args.get('user-id')
-    queue = channel.queue_declare(queue=queue_name, exclusive=False, durable=True)
-    messages = [queue.method.message_count]
-    if messages[0] > 0:
-        channel.basic_consume(
-            queue=queue_name,
-            on_message_callback=lambda *args, **kwargs: callback(messages, *args, **kwargs), 
-            auto_ack=True)
-        channel.start_consuming()
-    connection.close()
-    return "<p>All messages retrieved, see the log</p>"
+if __name__ == '__main__':
+    app.run(debug=False)
