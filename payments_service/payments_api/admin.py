f32b7112d7e7bab404ca79a5e04fa47bd34dcde2
AGrieco96 <100705990+AGrieco96@users.noreply.github.com>
2022-09-04 18:35:57 +0200

    Added Payments MicroService v0

---
+++ payments_service/payments_api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Product
+
+#Register your model here
+
+
+
+admin.site.register(Product)
---
+++ payments_service/payments_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PaymentsApiConfig(AppConfig):
+    name = 'payments_api'
