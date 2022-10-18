4117d8b8acab56a01e8a07d1c100fe966fbc6d65
nardoz-dev <nardomen96@gmail.com>
2022-10-18 16:56:01 +0200

    resolve conflit

--- frontend/src/pages/OrdersMainPage.js
+++ frontend/src/pages/OrdersMainPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -14,9 +14,8 @@ let total_price = 69.99;
 function OrdersMainPage(props) {
 
     let email;
+    const [orders, setOrders] = useState([]);
 
-    let tableBody = document.querySelector('tbody');
-    let out = '';
 
     // Authentication data from context storage
     const { auth } = useAuth();
@@ -25,31 +24,49 @@ function OrdersMainPage(props) {
     const axiosPrivate = useAxiosPrivate();
 
     useEffect(() => {
-        axiosUsers
-            .get(`users/${userId}/`)
-            .then((res) => {
-              email = res.data.email;
-              console.log(email)
-              axiosOrder.getOrder(email)
-              .then((res) => {
-                for ( const elem of res.data ){
-                    out +=  ` 
-                        <tr>
-                            <td> ${elem.id} </td>
-                            <td> ${elem.email} </td>
-                            <td> ${elem.payment_method_id} </td>
-                            <td> ${elem.price} </td>
-                        </tr>
-                        ` ;
-                    }
-                    console.log(out);
-                    tableBody.innerHTML = out;
-                });
-            })
-            .catch((error) => {
-                console.log(error.response)
-            })
+        (async () => {
+            await axiosPrivate
+                .get(`users/${userId}/`)
+                .then((res) => {
+                    email = res.data[0].email;
+                        axiosOrder.getOrder(email)
+                            .then((res) => {
+                                setOrders(res.data)
+                                });
+                            })
+                            .catch((error) => {
+                                console.log(error.response)
+                            })
+        })();
+    }, []); 
+    
 
+
+    const orders_array = orders.map((order) => {
+        return ( 
+            // <tr key={order.id}>
+            //     <td> {order.id} </td>
+            //     <td> {order.email} </td>
+            //     <td> {order.payment_method_id} </td>
+            //     <td> {order.price} </td>
+            // </tr>
+            <li className="list-group-item">
+                <div className="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div className="media-body order-2 order-lg-1">
+                        <h5 className="mt-0 font-weight-bold mb-2">Order N#:{order.payment_method_id}</h5>
+                        <p className="font-italic text-muted mb-0 small"></p>
+                        <div className="d-flex align-items-center justify-content-between mt-1">
+                            <h6 className="font-weight-bold my-2">Total : {order.price}</h6>
+                            <ul className="list-inline small">
+                                <Link className="btn btn-primary m-1" to={"orders/"} id="orders" >
+                                    Click to see More
+                                </Link>
+                            </ul>
+                        </div>
+                    </div>
+                </div> 
+            </li> 
+        );
     });
 
     return (
@@ -59,78 +76,12 @@ function OrdersMainPage(props) {
                     <h1 className="display-4">Order List</h1>
                 </div>
             </div>
-
-            <table className="fl-table">
-                <thead>
-                <tr>
-                    <th>OderID</th>
-                    <th>Done By User</th>
-                    <th>PaymentMethod</th>
-                    <th>Price</th>
-                </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
-
+            
             <div className="row">
                 <div className="col-lg-8 mx-auto">
                
                     <ul className="list-group shadow">
-                      
-                        <li className="list-group-item">
-                           
-                            <div className="media align-items-lg-center flex-column flex-lg-row p-3">
-                                <div className="media-body order-2 order-lg-1">
-                                    <h5 className="mt-0 font-weight-bold mb-2">Order N#:{id_order}</h5>
-                                    <p className="font-italic text-muted mb-0 small"></p>
-                                    <div className="d-flex align-items-center justify-content-between mt-1">
-                                        <h6 className="font-weight-bold my-2">Total : $ {total_price}</h6>
-                                        <ul className="list-inline small">
-                                            <Link className="btn btn-primary m-1" to={"orders/"} id="orders" >
-                                                Click to see More
-                                            </Link>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div> 
-                        </li> 
-                                            
-                                              
-                        <li className="list-group-item">
-                           
-                            <div className="media align-items-lg-center flex-column flex-lg-row p-3">
-                                <div className="media-body order-2 order-lg-1">
-                                    <h5 className="mt-0 font-weight-bold mb-2">Order N#:{id_order}</h5>
-                                    <p className="font-italic text-muted mb-0 small"></p>
-                                    <div className="d-flex align-items-center justify-content-between mt-1">
-                                        <h6 className="font-weight-bold my-2">Total : $ {total_price}</h6>
-                                        <ul className="list-inline small">
-                                            <Link className="btn btn-primary m-1" to={"orders/"} id="orders" >
-                                                Click to see More
-                                            </Link>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div> 
-                        </li> 
-                                              
-                        <li className="list-group-item">
-                           
-                            <div className="media align-items-lg-center flex-column flex-lg-row p-3">
-                                <div className="media-body order-2 order-lg-1">
-                                    <h5 className="mt-0 font-weight-bold mb-2">Order N#:{id_order}</h5>
-                                    <p className="font-italic text-muted mb-0 small"></p>
-                                    <div className="d-flex align-items-center justify-content-between mt-1">
-                                        <h6 className="font-weight-bold my-2">Total : $ {total_price}</h6>
-                                        <ul className="list-inline small">
-                                            <Link className="btn btn-primary m-1" to={"orders/"} id="orders" >
-                                                Click to see More
-                                            </Link>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div> 
-                        </li> 
+                        {orders_array}
                     </ul> 
                 </div>
             </div>
