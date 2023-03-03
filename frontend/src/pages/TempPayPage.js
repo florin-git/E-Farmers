eb42bb36a1fd60dddd0e83fabb988a363024cdd3
Marco Settanni <marcosettanni08@gmail.com>
2023-03-03 20:14:46 +0100

    fix template

--- frontend/src/pages/TempPayPage.js
+++ frontend/src/pages/TempPayPage.js
@@ -1,8 +1,14 @@
 //import PaymentForm from "./components/PaymentForm"
-
-import React, { useEffect, useState } from 'react';
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from "@stripe/stripe-js/pure";
+import React, {
+    useEffect,
+    useState
+} from 'react';
+import {
+    Elements
+} from '@stripe/react-stripe-js';
+import {
+    loadStripe
+} from "@stripe/stripe-js/pure";
 import CheckoutForm from "../components/CheckoutForm";
 
 import axiosOrder from '../api/axiosOrder';
@@ -15,80 +21,91 @@ import useAuth from "../hooks/useAuth";
 function TempPayPage() {
 
     let price = 0;
-/**
- * VARIABLES
- */
+    /**
+     * VARIABLES
+     */
 
-const stripePromise = loadStripe('pk_test_51LeK0EKut8s98N77mKeii9wLpQV1aG6HoNfQr2x8qjMu9bg3B5igb2ADSklcApQe5GU5tvuBaeeeuFX7bw4MdIA500A3nhBadE');
-const { auth } = useAuth();
-const userId = auth.userId;
-const [cart, setCart] = useState([]);
-const [boxes, setBoxes] = useState([]);
+    const [stripePromise, setStripePromise] = useState(() => loadStripe('pk_test_51LeK0EKut8s98N77mKeii9wLpQV1aG6HoNfQr2x8qjMu9bg3B5igb2ADSklcApQe5GU5tvuBaeeeuFX7bw4MdIA500A3nhBadE'))
 
-/**
- * FUNCTIONS
-*/
+    const {
+        auth
+    } = useAuth();
+    const userId = auth.userId;
+    const [cart, setCart] = useState([]);
+    const [boxes, setBoxes] = useState([]);
+
+    console.log('USER ID ' + userId);
+
+    /**
+     * FUNCTIONS
+     */
 
     useEffect(() => {
         (async () => {
             /**Get Cart from userId */
             await axiosInstance
-            .get(`users/${userId}/cart/`)
-            .then((res) => {
-                setCart(res.data);
-                axiosInstance
-                    .get(`users/${cart.user}/cart/items/`)
-                    .then((res) => {
-                        setBoxes(res.data);
-                    })
-                    // catch get boxes
-                    .catch((error) => {
-                        return error.response;
-                    })
-            // catch get cart
-            }).catch((error) => {
-                console.log(error);
-                alert('ERROR: couldn\'t get cart with userId ' + userId);
-            })
+                .get(`users/${userId}/cart/`)
+                .then((res) => {
+                    setCart(res.data);
+                    axiosInstance
+                        .get(`users/${userId}/cart/items/`)
+                        .then((res) => {
+                            setBoxes(res.data);
+                        })
+                        // catch get boxes
+                        .catch((error) => {
+                            return error.response;
+                        })
+                    // catch get cart
+                }).catch((error) => {
+                    console.log(error);
+                    alert('ERROR: couldn\'t get cart with userId ' + userId);
+                })
         })();
     }, [cart]);
 
     const boxes_array = boxes.map((box) => {
-        return (<CartItem key={box.id} {...box} />);
-    })
+            return ( < CartItem key = {
+                    box.id
+                } {
+                    ...box
+                }
+                />);
+            })
+
+        /*
+        function get_prices(boxes) {
+            let current_price = 0;
+            boxes.forEach(box => {
+                current_price += box.price
+            });
+            return current_price;
+        }
+
+        price = get_prices(boxes_array);
+        console.log(price);
+        */
+
+        return ( 
+            <Elements stripe = {stripePromise}>
+                <CheckoutForm price = {price}/> 
+            </Elements>
+        );
+
+    };
+    export default TempPayPage;
+
 
     /*
-    function get_prices(boxes) {
-        let current_price = 0;
-        boxes.forEach(box => {
-            current_price += box.price
-        });
-        return current_price;
+    function TempPayPage(props){
+
+     Ci arriviamo dal carrello quindi devo fare una get del cazzo per prendere i dati che devo mandare al form 
+
+        return (
+            <Elements stripe={stripePromise}>
+                <PaymentForm price="1000" />
+            </Elements>
+        )
     }
-
-    price = get_prices(boxes_array);
-    console.log(price);
+    export default TempPayPage;
     */
-
-    return(
-        <Elements stripe = {stripePromise}>
-            <CheckoutForm price={price} />
-        </Elements>
-    );
-
-};export default TempPayPage;
-
-
-/*
-function TempPayPage(props){
-
- Ci arriviamo dal carrello quindi devo fare una get del cazzo per prendere i dati che devo mandare al form 
-
-    return (
-        <Elements stripe={stripePromise}>
-            <PaymentForm price="1000" />
-        </Elements>
-    )
-}
-export default TempPayPage;
-*/
